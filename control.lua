f446889966d4d3ff1c01448ec4195af81c48a8a2
tistel <tistel.nilsson@gmail.com>
2018-10-05 21:37:40 +0200

    Version 0.1.3

--- control.lua
+++ control.lua
@@ -1,14 +1,13 @@
-require "defines"
-
 script.on_event(defines.events.on_player_created, function(event)
-	local player = game.get_player(event.player_index)
+    local player = game.players[event.player_index]
     player.clear_items_inside()
   	player.insert{name="iron-axe", count=5}
-		player.insert{name="burner-mining-drill", count=2}
-		player.insert{name="furnace-1", count=1}
-    player.insert{name="crusher-1", count=1}
-    player.insert{name="sifter-1", count=1}
-    player.insert{name="offshore-pump", count=1}
+		player.insert{name="burner-mining-drill", count=6}
+		player.insert{name="furnace-1", count=5}
+    player.insert{name="crusher-1", count=3}
+    player.insert{name="sifter-1", count=3}
+    player.insert{name="offshore-pump", count=3}
+    player.insert{name="iron-chest", count=5}
     player.insert{name="pistol", count=1}
-    player.insert{name="basic-bullet-magazine", count=10}
+    player.insert{name="basic-bullet-magazine", count=100}
 end)
